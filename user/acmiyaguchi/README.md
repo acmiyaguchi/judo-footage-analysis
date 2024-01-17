99e6976255d54cc617dfba73e825c87c91cf7674
Anthony Miyaguchi <acmiyaguchi@gatech.edu>
2024-01-17 18:54:26 +0000

    Add notebook for running inference and storing all data

--- user/acmiyaguchi/README.md
+++ user/acmiyaguchi/README.md
@@ -1,3 +1,15 @@
 # acmiyaguchi
 
 This is a directory for me to throw in random scripts or docs that may not belong elsewhere related to analysis.
+
+```bash
+./user/acmiyaguchi/run_detectron.py \
+    --input data/interim/mat-2-trunc.mp4 \
+    --output data/processed/mat-2-viz.mp4 \
+    --output-data-root data/processed/mat-2-data \
+    --duration 5
+```
+
+```bash
+python3 -m b2 sync data/processed b2://acm-judo/data/analysis/processed
+```
--- user/acmiyaguchi/run_detectron.py
+++ user/acmiyaguchi/run_detectron.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
+import pickle
 from argparse import ArgumentParser
-from functools import lru_cache
+from dataclasses import dataclass
 from pathlib import Path
 
 import ffmpeg
 import numpy as np
-import torch
 from detectron2 import model_zoo
 from detectron2.config import get_cfg
 from detectron2.data import MetadataCatalog
-from detectron2.modeling import build_model
+from detectron2.engine import DefaultPredictor
 from detectron2.utils.visualizer import Visualizer
 
 
@@ -22,8 +22,12 @@ def parse_args():
     )
     parser.add_argument("--score-threshold", type=float, default=0.8)
     parser.add_argument("--model_device", type=str, default="cuda")
+    parser.add_argument("--framerate", type=int, default=16)
+    parser.add_argument("--start-time", type=int, default=0)
+    parser.add_argument("--duration", type=int, default=-1)
     parser.add_argument("--input", type=str, required=True)
     parser.add_argument("--output", type=str, required=True)
+    parser.add_argument("--output-data-root", type=str)
     return parser.parse_args()
 
 
@@ -36,12 +40,10 @@ def get_detectron_model(args):
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(args.config_file)
     # only keep the top 5 instances
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = args.score_threshold
-    model = build_model(cfg)
-    model.train(False)
+    model = DefaultPredictor(cfg)
     return model, cfg
 
 
-@lru_cache
 def probe_video_dim(input):
     """Probe the video dimensions."""
     probe = ffmpeg.probe(input)
@@ -53,62 +55,125 @@ def probe_video_dim(input):
     return width, height
 
 
-def get_ffmpeg_frames_array(
-    args, start_time: int, duration: int, frame_rate: int = 15
-) -> np.ndarray:
+def get_ffmpeg_input_async(args, start_time: int, duration: int, framerate: int):
     """Initialize the ffmpeg stream."""
-    width, height = probe_video_dim(args.input)
-
     # https://github.com/kkroening/ffmpeg-python/blob/master/examples/README.md#convert-video-to-numpy-array
     # https://stackoverflow.com/questions/63623398/read-one-16bits-video-frame-at-a-time-with-ffmpeg-python
-    out, _ = (
-        ffmpeg.input(args.input)
-        .trim(start_frame=start_time * frame_rate, duration=duration)
-        .filter("fps", fps=frame_rate, round="up")
-        .output("pipe:", format="rawvideo", pix_fmt="rgb24")
-        .run(capture_stdout=True)
+    input = ffmpeg.input(args.input).filter("fps", fps=framerate, round="up")
+    if duration > 0 or start_time > 0:
+        input = input.trim(start_frame=start_time * framerate, duration=duration)
+    return input.output("pipe:", format="rawvideo", pix_fmt="rgb24").run_async(
+        pipe_stdout=True
     )
-    video = (
-        np.frombuffer(out, np.uint8).reshape([-1, height, width, 3]).astype(np.float32)
+
+
+def get_ffmpeg_output_async(args, width: int, height: int, framerate: int):
+    """Initialize the ffmpeg stream."""
+    return (
+        ffmpeg.input(
+            "pipe:",
+            format="rawvideo",
+            pix_fmt="rgb24",
+            s=f"{width}x{height}",
+            framerate=framerate,
+        )
+        # https://stackoverflow.com/questions/10241433/slow-ffmpegs-images-per-second-when-creating-video-from-images
+        # this isn't the right way to do things, but i want something to work...
+        # .filter("setpts", f"(25/{framerate})*PTS")
+        .output(args.output, pix_fmt="yuv420p", vcodec="libx264")
+        .overwrite_output()
+        .run_async(pipe_stdin=True)
     )
-    return video
 
 
-class VideoDataset(torch.utils.data.Dataset):
-    def __init__(self, video: np.ndarray):
-        self.video = video
+class DataPickleWriter:
+    """Write a stream of data to a series of pickle files."""
 
-    def __getitem__(self, index):
-        # https://detectron2.readthedocs.io/en/latest/tutorials/models.html#model-input-format
-        # https://github.com/facebookresearch/detectron2/issues/282
-        return {"image": torch.from_numpy(self.video[index].transpose(2, 0, 1))}
+    def __init__(self, output_data_root: str, chunk_size=16 * 30):
+        self.output_data_root = Path(output_data_root)
+        self.chunk_size = chunk_size
+        self.chunk_index = 0
+        self.chunk = []
 
-    def __len__(self):
-        return len(self.video)
+    def write(self, data):
+        self.chunk.append(data)
+        if len(self.chunk) == self.chunk_size:
+            self.flush()
+
+    def flush(self):
+        self.output_data_root.mkdir(parents=True, exist_ok=True)
+        # use 4 digits for the chunk
+        with (self.output_data_root / f"chunk_{self.chunk_index:04d}.pkl").open(
+            "wb"
+        ) as fp:
+            pickle.dump(self.chunk, fp)
+        self.chunk_index += 1
+        self.chunk = []
+
+
+@dataclass
+class InferenceData:
+    frame_index: int
+    framerate: int
+    image: np.ndarray
+    instances: object
 
 
 def main():
     args = parse_args()
+    # this is actually the DefaultPredictor
     model, cfg = get_detectron_model(args)
-    video = get_ffmpeg_frames_array(args, start_time=0, duration=30, frame_rate=1)
+    width, height = probe_video_dim(args.input)
 
-    dataset = VideoDataset(video)
-    dataloader = torch.utils.data.DataLoader(
-        dataset, batch_size=15, collate_fn=lambda x: x
+    in_stream = get_ffmpeg_input_async(
+        args,
+        start_time=args.start_time,
+        duration=args.duration,
+        framerate=args.framerate,
     )
-    for batch in dataloader:
-        predictions = model(batch)
+    out_stream = get_ffmpeg_output_async(args, width, height, framerate=args.framerate)
+
+    # process frame by frame
+    if args.output_data_root:
+        pickle_writer = DataPickleWriter(args.output_data_root)
+        print(f"writing data to {args.output_data_root}")
+    else:
+        pickle_writer = None
+    index = 0
+    while True:
+        in_bytes = in_stream.stdout.read(width * height * 3)
+        if not in_bytes:
+            break
+
+        in_frame = np.frombuffer(in_bytes, np.uint8).reshape([height, width, 3])
+        prediction = model(in_frame)
+        instances = prediction["instances"].to("cpu")
         # now write out an image to test
-        for i, prediction in enumerate(predictions):
-            v = Visualizer(
-                batch[i]["image"].permute(1, 2, 0).numpy(),
-                metadata=MetadataCatalog.get(cfg.DATASETS.TRAIN[0]),
-                scale=1.2,
+        v = Visualizer(
+            in_frame[:, :, ::-1],
+            metadata=MetadataCatalog.get(cfg.DATASETS.TRAIN[0]),
+            scale=1,
+        )
+        out_frame = v.draw_instance_predictions(instances)
+        out_stream.stdin.write(
+            out_frame.get_image()[:, :, ::-1].astype(np.uint8).tobytes()
+        )
+        if pickle_writer:
+            pickle_writer.write(
+                InferenceData(
+                    frame_index=index,
+                    framerate=args.framerate,
+                    image=in_frame,
+                    instances=instances,
+                )
             )
-            # print(prediction["instances"])
-            out = v.draw_instance_predictions(prediction["instances"].to("cpu"))
-            out.save(f"data/interim/test/output_{i}.jpg")
-        break
+        index += 1
+
+    if pickle_writer:
+        pickle_writer.flush()
+    out_stream.stdin.close()
+    out_stream.wait()
+    in_stream.wait()
 
 
 if __name__ == "__main__":
