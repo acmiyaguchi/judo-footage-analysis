618ee576cc5e9918eaafb5db45f2c1736429a2e9
SUTAR <tsutar@gtlpc111.georgiatech-metz.fr>
2024-03-19 17:28:14 +0100

    adding script for generating overlay

---
+++ user/tsutar/scripts/train_yolo_entity_detection.py
@@ -0,0 +1,15 @@
+from ultralytics import YOLO
+
+# import torch
+
+# Load a model
+model = YOLO("yolov8m.pt")  # load a pretrained model (recommended for training)
+# print(torch.cuda.devices())
+
+# Train the model
+results = model.train(
+    data="/home/GPU/tsutar/home_gtl/intro_to_res/cs8813-judo-footage-analysis/user/tsutar/scripts/yolo_train_entity_detection_config.yaml",
+    epochs=50,
+    imgsz=640,
+    device=0,
+)
