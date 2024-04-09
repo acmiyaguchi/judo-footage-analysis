aa3f15d28f4d9b04e7b3d2feb4cf066613a4005d
tanny2109 <tsutar3@gatech.edu>
2024-04-09 17:04:41 +0200

    Adding referee classification code

--- user/tsutar/scripts/test_yolo_inference.py
+++ user/tsutar/scripts/test_yolo_inference.py
@@ -6,32 +6,17 @@ from pathlib import Path
 import luigi
 from ultralytics import YOLO
 
-model_name = "/home/GTL/tsutar/intro_to_res/cs8813-judo-footage-analysis/user/tsutar/runs/classify/train10/weights/last.pt"
+model_name = "/cs-share/pradalier/tmp/judo/models/referee_pose/v1/weights/03-04-2024-referee-best.pt"
 yolo_original = (
     "/home/GTL/tsutar/intro_to_ress8813-judo-footage-analysis/user/tsutar/yolov8n.pt"
 )
 model = YOLO(model_name)
 
-input_path = "/mnt/cs-share/pradalier/tmp/judo/frames/mat_01/0001/0001.jpg"
+input_path = "/cs-share/pradalier/tmp/judo/data/referee_v2/mat_01/0000/0398_00.png"
 
-results = model(input_path, stream=False, verbose=False)
+results = model(input_path, verbose=False)
 
 result_dict = []
 # Process results list
 for result in results:
-    path = result.path
-    names = result.names
-    probs = result.probs.cpu().numpy()
-    class_probs = probs.data  # Probs object for classification outputs
-    class_list = class_probs.tolist()
-    print(type(class_list))
-    names["path"] = path
-    names["prob"] = class_list
-    result_dict.append(names)
-
-with open("output.json", "w") as outfile:
-    for r in result_dict:
-        json.dump(r, outfile)
-
-# for r in result_dict:
-#     print(r)
+    print(result.probs)
